cbef693e948f5bbfb51d1ff9e89d1c68e757828d
Guy Guetta <guetta@microfocus.com>
2022-06-27 13:47:33 +0300

    test pr

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
-    Name = "Guy Test PR"
+    Name = "Guy and friends"
   }
 
   user_data = <<-EOF
